54571297e71d582527ea219331b1597975c8cde1
DanielNery <danielpontesnery@gmail.com>
2023-11-21 05:19:07 -0300

    mandando tudo.

---
+++ blob.py
@@ -0,0 +1,16 @@
+from textblob import TextBlob
+
+text = '''
+Eu amo isso
+'''
+
+blob = TextBlob(text)
+blob.tags           # [('The', 'DT'), ('titular', 'JJ'),
+                    #  ('threat', 'NN'), ('of', 'IN'), ...]
+
+blob.noun_phrases   # WordList(['titular threat', 'blob',
+                    #            'ultimate movie monster',
+                    #            'amoeba-like mass', ...])
+
+for sentence in blob.sentences:
+    print(sentence.sentiment.polarity)
